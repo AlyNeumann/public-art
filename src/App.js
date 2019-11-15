7d66f8cbfc994540115881a55ea62b20b9bba9a6
Allison Neumann <allisonneumann@Allisons-MacBook-Pro.local>
2019-11-15 18:16:46 -0500

    map working with JSON coordinates, custom style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import ReactMapGL, {Marker} from "react-map-gl";
+import dotenv from 'dotenv';
+import * as artData from "./data/art.json";
+// import * as artData from "./data/skateboard-parks.json";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  dotenv.config();
+  const [viewport, setViewport] = useState({
+    //OTTAWA
+    // latitude: 45.4211,
+    // longitude: -75.6903,
+    //MTL
+    latitude: 45.485270,
+    longitude: -73.581420,
+    width: '100vw',
+    height: '100vh',
+    zoom: 10
+  })
+  const markerStyle = {
+    fontSize: '15px',
+    color: 'black'
+  }
+ return(
+   <div>
+     <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/alymarguerite/ck30gb37i0ynh1cm8m7xlk465"
+      // mapStyle="mapbox://styles/alymarguerite/ck30gwnu40z7y1cm8lumnxcoe"
+      onViewportChange={viewport => {
+          setViewport(viewport);
+      }}
+      >
+        {artData.features.map((art) => (
+          <Marker 
+          key={art.NoInterne}
+          longitude={JSON.parse(art.CoordonneeLongitude)}
+          latitude={JSON.parse(art.CoordonneeLatitude)}
+          >
+            <div style={markerStyle}>ART</div>
+          </Marker>
+        ))}
+     </ReactMapGL>
+   </div>
+ );
 }
 
 export default App;
